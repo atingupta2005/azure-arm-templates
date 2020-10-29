82d61013cd4d6ab85c62baef40a0988c10e32409
Atin Gupta <atingupta2005@gmail.com>
2020-10-29 09:25:01 +0530

    -

--- 1-create-vm.ps1
+++ 1-create-vm.ps1
@@ -4,10 +4,10 @@
 #Get-AzSubscription -SubscriptionId "83c07bcb-1ba8-4fb5-beb5-2a30c680d4fc" -TenantId "6bb2f9af-a0af-4c32-a5ec-5f7011d37551" | Set-AzContext
 
 #-------------#First Time---------------------------------
-$resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
-$location = Read-Host -Prompt "Enter the Location"
-$Virtual_Machine_Suffix = Read-Host -Prompt "Virtual Machine_Suffix (vm)"
-$vmCount = Read-Host -Prompt "How many VMs to Create? "
+$resourceGroupName = Read-Host -Prompt "Enter the Resource Group name (Mandatory):"
+$location = Read-Host -Prompt "Enter the Location (Mandatory):"
+$Virtual_Machine_Suffix = Read-Host -Prompt "Virtual Machine_Suffix (Mandatory):"
+$vmCount = Read-Host -Prompt "How many VMs to Create(Mandatory)?:"
 
 #notepad parameters.json 	# Edit this parameters file and specify your custom values in it
 
